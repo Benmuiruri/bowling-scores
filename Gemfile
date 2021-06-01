c0344ff7691978acc0a2736d33eb43471df499c7
Yonny Alexander Quiceno Tapias <yonnyquiceno@gmail.com>
2021-06-01 16:11:53 -0500

    Add Gemfile.lock

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source 'https://rubygems.org'
 ruby '2.7.3'
 
 gem 'rspec'
-gem 'rubocop'
-gem 'rubocop-rspec', require: false
+gem 'rubocop', '~> 1.16', require: false
+gem 'rubocop-rspec', '~> 2.3.0', require: false
--- spec/lib/main_spec.rb
+++ spec/lib/main_spec.rb
@@ -1,19 +1,19 @@
-require 'spec_helper.rb'
-require 'main.rb'
+require 'spec_helper'
+require 'main'
 
 RSpec.describe Main do
   context 'when input file is valid' do
-    context 'and the game has more than two players' do
+    context 'with more than two players' do
       xit 'prints the game scoreboard to stdout' do
       end
     end
 
-    context 'and all frames are strikes' do
+    context 'with strikes in all throwings' do
       xit 'prints a perfect game scoreboard' do
       end
     end
 
-    context 'and all frames are fouls' do
+    context 'with fouls in all throwings' do
       xit 'prints the game scoreboard to stdout' do
       end
     end
@@ -21,15 +21,18 @@ RSpec.describe Main do
 
   context 'when input file is invalid' do
     context 'with invalid characters present' do
-      xit 'raises the corresponding error message'
+      xit 'raises the corresponding error message' do
+      end
     end
 
     context 'with invalid score' do
-      xit 'raises the corresponding error message'
+      xit 'raises the corresponding error message' do
+      end
     end
 
     context 'with invalid number of throwings' do
-      xit 'raises the corresponding error message'
+      xit 'raises the corresponding error message' do
+      end
     end
   end
 end
